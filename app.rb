61eb5a82ca3a1c434cb668ea77cb13d79f214602
lacostenycoder <djlancejordan@gmail.com>
2015-03-08 15:52:18 -0400

    add readme. add text output color and formatting

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 $LOAD_PATH << '.'
 
-
-puts 'Welcome to the MASSIVE number tranlator'.upcase
-puts 'Enter any number up to one hundred vigintillion to translate: '
-answer = gets.chomp
+puts `clear`
+puts 'Welcome to the MASSIVE number tranlator'.upcase + "\n\r"
+puts "Enter any number up to one hundred vigintillion to translate: \n\r"
+answer = gets.chomp.gsub(/\D/,'')
 require 'word_number'
 require 'fixnum'
 require 'bignum'
 number = WordNumber.new(number: answer.to_i)
-STDOUT.flush
-puts 'Your number #{answer} is: ' + number.to_word(answer.to_i)
+string = "Your number #{answer} in English is: \n\n" + number.to_word(answer.to_i)
+puts "\n\r\033[1m**** SUCCESS!!! ****\033[0m\n\n\r"
+puts  "\033[35m#{string.upcase} \033[0m\n\n\r"
